5e25e7fb4645ec7a06994058176bbe5d5fee0dd5
liuyongdi <wangchuanqiushui2000@gmail.com>
2022-10-29 16:19:24 +0800

    all

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.a16a81ca9ec3e315ebdbb905f98d2750.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "pptist"});
+
+workbox.core.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/.*/, new workbox.strategies.NetworkFirst({ "cacheName":"PPTist", plugins: [new workbox.expiration.Plugin({ maxAgeSeconds: 36000, purgeOnQuotaError: false }), new workbox.cacheableResponse.Plugin({ statuses: [ 0, 200 ] })] }), 'GET');
